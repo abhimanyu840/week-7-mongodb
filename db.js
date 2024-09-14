15b749ac829e633b8691b0b56b1e8eb75429a3e2
Abhimanyu Kumar <akabhimanyukumar111@gmail.com>
2024-09-14 21:34:52 +0530

    initial commit

--- db.js
+++ db.js
@@ -5,20 +5,22 @@ const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
   name: String,
-  email: {type: String, unique: true},
-  password: String
+  email: { type: String, unique: true, validate: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ },
+  password: { type: String },
 });
 
 const Todo = new Schema({
-    userId: ObjectId,
-    title: String,
-    done: Boolean
+  userId: { type: ObjectId, ref: 'users' },
+  title: String,
+  done: Boolean,
+  createdAt: { type: Date, default: Date.now },
+  dueAt: Date
 });
 
 const UserModel = mongoose.model('users', User);
 const TodoModel = mongoose.model('todos', Todo);
 
 module.exports = {
-    UserModel,
-    TodoModel
+  UserModel,
+  TodoModel
 }
--- index.js
+++ index.js
@@ -3,39 +3,48 @@ const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/")
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function(req, res) {
+app.post("/signup", async function (req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
-    
-    res.json({
-        message: "You are signed up"
-    })
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+
+        res.json({
+            message: "You are signed up"
+        })
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+
 });
 
 
-app.post("/signin", async function(req, res) {
+app.post("/signin", async function (req, res) {
     const email = req.body.email;
     const password = req.body.password;
 
     const response = await UserModel.findOne({
-        email: email,
-        password: password,
+        email: email
     });
 
-    if (response) {
+    if (response && await bcrypt.compare(password, response.password)) {
         const token = jwt.sign({
             id: response._id.toString()
         }, JWT_SECRET);
@@ -51,7 +60,7 @@ app.post("/signin", async function(req, res) {
 });
 
 
-app.post("/todo", auth, async function(req, res) {
+app.post("/todo", auth, async function (req, res) {
     const userId = req.userId;
     const title = req.body.title;
     const done = req.body.done;
@@ -59,7 +68,8 @@ app.post("/todo", auth, async function(req, res) {
     await TodoModel.create({
         userId,
         title,
-        done
+        done,
+        dueAt: new Date()
     });
 
     res.json({
@@ -68,7 +78,7 @@ app.post("/todo", auth, async function(req, res) {
 });
 
 
-app.get("/todos", auth, async function(req, res) {
+app.get("/todos", auth, async function (req, res) {
     const userId = req.userId;
 
     const todos = await TodoModel.find({
@@ -77,6 +87,20 @@ app.get("/todos", auth, async function(req, res) {
 
     res.json({
         todos
+    })
+});
+
+app.put("/todo/:id", auth, async function (req, res) {
+    const id = req.params.id;
+
+    await TodoModel.findOneAndUpdate({
+        _id: id,
+        done: true
+    });
+
+
+    res.json({
+        message: "Todo updated"
     })
 });
 
